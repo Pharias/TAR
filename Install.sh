74796fc3239c2d32b5f8805de495db9526a594f8
that1chopperguy <66369207+that1chopperguy@users.noreply.github.com>
2021-03-21 14:19:28 +0100

    update der scripte

--- Install.sh
+++ Install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "**********************************************************************" &&
+echo "" &&
 echo "diese Anwendung intalliert docker, homeassistant, mqtt und zigbee2mqtt" &&
 echo "stellen sie sicher das dieses Script durch user pi ausgefürt wird" &&
+echo "" &&
+echo "**********************************************************************" &&
 
 #raspberry updaten
 sudo apt-get update &&
@@ -11,7 +15,8 @@ echo "System erfolgreich aktualisiert" &&
 #docker installieren
 cd /home/pi &&
 FILE=docker-script.sh
-if [ -f "$FILE" ]; then
+if [ -f "$FILE" ]
+then
     echo " $FILE existiert bereits."
     sudo sh docker-script.sh
     sudo usermod -aG docker pi
@@ -27,7 +32,8 @@ fi
 #verzeichnisse erstellen
 cd /home/pi &&
 FILE=/home/pi/docker
-if [ -f "$FILE" ]; then
+if [ -f "$FILE" ]
+then
     echo "verzeichnis $FILE existiert bereits."
 else 
     mkdir /home/pi/docker 
@@ -37,7 +43,8 @@ fi
 cd /home/pi/docker &&
 
 FILE=/home/pi/docker/homeassistant
-if [ -f "$FILE" ]; then
+if [ -f "$FILE" ]
+then
     echo "verzeichnis $FILE existiert bereits."
 else 
     mkdir homeassistant
@@ -45,7 +52,8 @@ else
 fi
 
 FILE=/home/pi/docker/mqtt
-if [ -f "$FILE" ]; then
+if [ -f "$FILE" ]
+then
     echo "verzeichnis $FILE existiert bereits."
 else 
     mkdir mqtt
@@ -53,7 +61,8 @@ else
 fi
 
 FILE=/home/pi/docker/zigbee2mqtt
-if [ -f "$FILE" ]; then
+if [ -f "$FILE" ]
+then
     echo "verzeichnis $FILE existiert bereits."
 else 
     mkdir zigbee2mqtt
--- install_hassio.bash
+++ install_hassio.bash
@@ -53,10 +53,110 @@ then
 	# Hier Python installieren
 fi
 
-# Installation der Prerequisites
+# Installation der Prerequisites verzeichnisse erstellen
+while getopts "" arg; do
+	case $arg in
+			
+	esac
 
+ Prüfe Python-Version und installiere ggf. die neue Version
+ python_output=$(python --version 2>&1)
+ python_version=$(echo $python_output | cut -d' ' -f2)
+ if[$python_version >= 3.9.0 ]
+ then
+	echo "Veraltete Python-Version gefunden. Installiere Python Version 3.9.2..."
+	# Hier Python installieren
+fi
+
+# Installation der Prerequisites verzeichnisse erstellen
+sudo apt-get update &&
+sudo apt-get upgrade &&
+echo "System erfolgreich aktualisiert." &&
+
+FILE=/home/pi/docker
+if [ -e "$FILE" ]
+then 
+    echo "verzeichnis $FILE existiert bereits."
+else 
+    mkdir /home/pi/docker 
+    echo "verzeichnis $FILE wurde erstellt."
+	cd /home/pi/docker
+fi
+
+FILE=/home/pi/docker/homeassistant
+if [ -e "$FILE" ]
+then 
+    echo "verzeichnis $FILE existiert bereits."
+else 
+    mkdir homeassistant
+    echo "verzeichnis $FILE wurde erstellt."
+fi
+
+FILE=/home/pi/docker/mqtt
+if [ -e "$FILE" ]
+then 
+    echo "verzeichnis $FILE existiert bereits."
+else 
+    mkdir mqtt
+    echo "verzeichnis $FILE wurde erstellt."
+fi
+
+FILE=/home/pi/docker/zigbee2mqtt
+if [ -e "$FILE" ]
+then 
+    echo "verzeichnis $FILE existiert bereits."
+else 
+    mkdir zigbee2mqtt
+    echo "verzeichnis $FILE wurde erstellt."
+fi
+
+# Installation Docker
+
+cd /home/pi &&
+FILE=docker-script.sh
+if [ -f "$FILE" ]
+then
+    echo " $FILE existiert bereits."
+    sudo sh docker-script.sh
+    sudo usermod -aG docker pi
+    echo "docker wurde installiert"
+else 
+    curl -fsSL https://get.docker.com -o docker-script.sh
+    sudo sh docker-script.sh
+    sudo usermod -aG docker pi
+    echo "docker wurde installiert"
+fi
 
 # Installation Home-Assistants
 
+FILE=/home/pi/docker/homeassistant
+if [ -e "$FILE" ]
+then 
+	docker run --init -d \
+  	    --name homeassistant \
+  	    --restart=unless-stopped \
+  	    -v /etc/localtime:/etc/localtime:ro \
+  	    -v /home/pi/docker/homeassistant:/config \
+  	    --network=host \
+  	    homeassistant/raspberrypi3-homeassistant:stable
+        #für RBPI4
+        #homeassistant/raspberrypi4-homeassistant:stable
+	echo "Home Assistant wurde erfolgreich installiert"
+else 
+    echo "fehler"
+fi
+
+# Installation von MQTT
+
+# Installation von Zigbee2MQTT
+
+docker run \
+   -it \
+   -v /home/pi/docker/zigbee2mqtt \
+   --device=/dev/ttyACM0 \              #Ort des CC2531
+   -e TZ=Europe/Berlin \
+   -v /run/udev:/run/udev:ro \
+   --privileged=true \
+   koenkk/zigbee2mqtt
 
 # für später: Installation MyCroft
