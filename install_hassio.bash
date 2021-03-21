5fe1eee21e96cbd10b4825b38dad9fb2ec958b64
Pharias <narfmaster@rocketmail.com>
2021-03-21 16:01:14 +0100

    Update install_hassio.bash
    Felix will Skript

--- install_hassio.bash
+++ install_hassio.bash
@@ -4,11 +4,12 @@
 # 
 
 # Variablen
-
-
-# Init
-FILE="/tmp/out.$$"
-GREP="/bin/grep"
+DOCKER=/home/pi/docker
+HOMEASSISTANT=/home/pi/docker/homeassistant
+MQTT=/home/pi/docker/mqtt
+ZIGBEE=/home/pi/docker/zigbee2mqtt
+DOCKERSCRIPT=docker-script.sh
+PIVERSION="${cat /proc/device-tree/model}"
 
 # Prüft ob Root:
 if [[ $EUID -ne 0 ]]; then
@@ -18,105 +19,83 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Erstellen der Hilfe-Funktion
-Help()
-{
-	echo "Skript zur Installation des Home Assistants auf Raspberry OS mit Zigbee2MQTT"
-	echo 
-	echo "Zur Ausführung des Skripts werden root-Rechte benötigt, bitte führen Sie das Skript nur als entsprechender Nutzer aus."
-	echo 
-	echo "Syntax: install_hassio.bash [-h] [parameter] "
-	echo "Optionen:"
-	echo " h		Zeigt diese Hilfemeldung an"
-	echo
-
-}
+# Help()
+#{
+#	echo "Skript zur Installation des Home Assistants auf Raspberry OS mit Zigbee2MQTT"
+#	echo 
+#	echo "Zur Ausführung des Skripts werden root-Rechte benötigt, bitte führen Sie das Skript nur als entsprechender Nutzer aus."
+#	echo 
+#	echo "Syntax: install_hassio.bash [-h] [parameter] "
+#	echo "Optionen:"
+#	echo " h		Zeigt diese Hilfemeldung an"
+#	echo
+#}
 
 # Prüfe, ob Hilfe angefordert wurde
-while getopts ":h" option; do
-   case $option in
-      h) Help
-         exit;;
-   esac
+#while getopts ":h" option; do
+#   case $option in
+#      h) Help
+#         exit;;
+#   esac
 
 # Einlesen der eventuell übergebenen Parameter
-while getopts "" arg; do
-	case $arg in
-			
-	esac
-
-# Prüfe Python-Version und installiere ggf. die neue Version
-python_output=$(python --version 2>&1)
-python_version=$(echo $python_output | cut -d' ' -f2)
-if[$python_version >= 3.9.0 ]
-then
-	echo "Veraltete Python-Version gefunden. Installiere Python Version 3.9.2..."
-	# Hier Python installieren
-fi
+#while getopts "" arg; do
+#	case $arg in
+#			
+#	esac
 
 # Installation der Prerequisites verzeichnisse erstellen
-while getopts "" arg; do
-	case $arg in
-			
-	esac
+#while getopts "" arg; do
+#	case $arg in
+#			
+#	esac
 
- Prüfe Python-Version und installiere ggf. die neue Version
- python_output=$(python --version 2>&1)
- python_version=$(echo $python_output | cut -d' ' -f2)
- if[$python_version >= 3.9.0 ]
- then
-	echo "Veraltete Python-Version gefunden. Installiere Python Version 3.9.2..."
+# Prüfe Python-Version und installiere ggf. die neue Version
+# python_output=$(python --version 2>&1)
+# python_version=$(echo $python_output | cut -d' ' -f2)
+# if[$python_version >= 3.9.0 ]
+# then
+#	echo "Veraltete Python-Version gefunden. Installiere Python Version 3.9.2..."
 	# Hier Python installieren
-fi
+#fi
 
 # Installation der Prerequisites verzeichnisse erstellen
 sudo apt-get update &&
 sudo apt-get upgrade &&
 echo "System erfolgreich aktualisiert." &&
 
-FILE=/home/pi/docker
-if [ -e "$FILE" ]
-then 
-    echo "verzeichnis $FILE existiert bereits."
+if [ -d "$DOCKER" ]; then 
+    echo "verzeichnis $DOCKER existiert bereits."
 else 
     mkdir /home/pi/docker 
-    echo "verzeichnis $FILE wurde erstellt."
-	cd /home/pi/docker
+    echo "verzeichnis $DOCKER wurde erstellt."
 fi
 
-FILE=/home/pi/docker/homeassistant
-if [ -e "$FILE" ]
-then 
-    echo "verzeichnis $FILE existiert bereits."
+if [ -d "$HOMEASSISTANT" ]; then 
+    echo "verzeichnis $HOMEASSISTANT existiert bereits."
 else 
     mkdir homeassistant
-    echo "verzeichnis $FILE wurde erstellt."
+    echo "verzeichnis $HOMEASSISTANT wurde erstellt."
 fi
 
-FILE=/home/pi/docker/mqtt
-if [ -e "$FILE" ]
-then 
-    echo "verzeichnis $FILE existiert bereits."
+if [ -d "$MQTT" ]; then 
+    echo "verzeichnis $MQTT existiert bereits."
 else 
     mkdir mqtt
-    echo "verzeichnis $FILE wurde erstellt."
+    echo "verzeichnis $MQTT wurde erstellt."
 fi
 
-FILE=/home/pi/docker/zigbee2mqtt
-if [ -e "$FILE" ]
-then 
-    echo "verzeichnis $FILE existiert bereits."
+if [ -d "$ZIGBEE" ]; then 
+    echo "verzeichnis $ZIGBEE existiert bereits."
 else 
     mkdir zigbee2mqtt
-    echo "verzeichnis $FILE wurde erstellt."
+    echo "verzeichnis $ZIGBEE wurde erstellt."
 fi
 
 # Installation Docker
-
-cd /home/pi &&
-FILE=docker-script.sh
-if [ -f "$FILE" ]
+if [ -f "$DOCKERSCRIPT" ]
 then
-    echo " $FILE existiert bereits."
+    echo " $DOCKERSCRIPT existiert bereits."
     sudo sh docker-script.sh
     sudo usermod -aG docker pi
     echo "docker wurde installiert"
@@ -128,24 +107,16 @@ else
 fi
 
 # Installation Home-Assistants
-
-FILE=/home/pi/docker/homeassistant
-if [ -e "$FILE" ]
-then 
-	docker run --init -d \
-  	    --name homeassistant \
-  	    --restart=unless-stopped \
-  	    -v /etc/localtime:/etc/localtime:ro \
-  	    -v /home/pi/docker/homeassistant:/config \
-  	    --network=host \
-  	    homeassistant/raspberrypi3-homeassistant:stable
+docker run --init -d \
+	--name homeassistant \
+  	--restart=unless-stopped \
+  	-v /etc/localtime:/etc/localtime:ro \
+  	-v /home/pi/docker/homeassistant:/config \
+  	--network=host \
+  	homeassistant/raspberrypi3-homeassistant:stable
         #für RBPI4
         #homeassistant/raspberrypi4-homeassistant:stable
-	echo "Home Assistant wurde erfolgreich installiert"
-else 
-    echo "fehler"
-fi
-
+echo "Home Assistant wurde erfolgreich installiert"
 # Installation von MQTT
 
 # Installation von Zigbee2MQTT
