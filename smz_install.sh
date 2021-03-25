1e62df243f3b10c8638f2362a678759c99e86ab1
Pharias <narfmaster@rocketmail.com>
2021-03-25 17:37:53 +0100

    skrippeteyskript

--- smz_install.sh
+++ smz_install.sh
@@ -19,23 +19,23 @@ DOCKER=0
 # Homeassistant-Flag
 HOMEASSISTANT=0
 # Dependencies-Flag
-DEPENDENCIES=1
+DEPENDENCIES=0
 # Zeitstempel
 TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
 # Logdatei
-LOGFILE=smz_install.log
+LOGFILE=shz_install.log
 echo "$TIMESTAMP: Skript gestartet." >> $LOGFILE
 #Versionsabfrage des Pi
-if[[$PIVER == "4" ]]; then
+if [[ "$PIVER" -eq 4 ]]; then
 	MACHINE=raspberrypi4
 	echo "$TIMESTAMP: Raspberry Pi Version $PIVER erkannt." >> $LOGFILE
-elif[[ $PIVER == "3" ]]; then
+elif [[ "$PIVER" -eq 3 ]]; then
 	MACHINE=raspberrypi3
 	echo "$TIMESTAMP: Raspberry Pi Version $PIVER erkannt." >> $LOGFILE
 else
-	# Fehlermeldung
+    # Fehlermeldung
 	echo "Pi Version unbekannt."
-	echo 
+	echo
 	echo "Sollte Ihr Pi älter sein als der Raspberry Pi 3, "
 	echo "besorgen Sie sich bitte neue Hardware."
 	echo "Wenn Sie einen neueren Raspberry Pi als Version 4"
@@ -43,60 +43,63 @@ else
 	echo "Skript abgebrochen. Pi-Version nicht erkannt/korrekt" >> $LOGFILE
 	exit;
 fi
-# Prüfe, ob Hilfe angefordert wurde
-while getopts ":h" option; do
-   case $option in
-      h) Help
-         exit;;
-   esac
 # Einlesen der eventuell übergebenen Parameter
-while getopts ":d:u:ha:nd:" arg; do
-	case $arg in
-		d) $DOCKER=1
-		u) $UPDATE=1
-		ha) $HOMEASSISTANT=1
-		nd) $DEPENDENCIES=0
+# FUNKTIONIERT NIT!
+while getopts ":d:u:ha:nd:h:" option; do
+	case $option in
+		d) DOCKER=1;;
+		u) UPDATE=1;;
+		ha) HOMEASSISTANT=1;;
+		nd) DEPENDENCIES=1;;
+		h) Help
+		   exit;;
 	esac
 done
 # Update bei Bedarf
-if[[ $UPDATE == "1" ]]; then
-	sudo apt update
-	sudo apt upgrade -y
+if [[ "$UPDATE" -eq 1 ]]; then
+	echo "$TIMESTAMP: Beginne mit Systemupdate" >> $LOGFILE
+	echo "Beginne mit Systemupdate..."
+	sudo apt update >> $LOGFILE
+	sudo apt upgrade -y >> $LOGFILE
 	echo "$TIMESTAMP: Update des Systems wurde erfolgreich abgeschlossen " >> $LOGFILE
+	echo "Systemupdate beendet."
 else
 	echo "$TIMESTAMP: Updates nicht ausgeführt - USERWAHL" >> $LOGFILE
 fi
 #installation von Docker
-if[[ $DOCKER == "1" ]]; then
-	curl -fsSL https://get.docker.com -o get-docker.sh
-	sudo sh get-docker.sh
+if [[ "$DOCKER" -eq 1 ]]; then
+	echo "$TIMESTAMP: Beginne mit Docker Installation" >> $LOGFILE
+	echo "Beginne mit Docker Installation..."
+	curl -fsSL https://get.docker.com -o get-docker.sh >> $LOGFILE
+	sudo sh get-docker.sh >> $LOGFILE
 	docker --version
-	echo "$TIMESTAMP: Docker wurde Erfolgreich installiert " >> $LOGFILE
+	echo "Docker installiert. Warte 20s."
+	echo "$TIMESTAMP: Docker wurde erfolgreich installiert " >> $LOGFILE
 	sleep 20s
 else
 	echo "$TIMESTAMP: Docker nicht installiert - USERWAHL" >> $LOGFILE
 fi
 # Installiert Dependencies sofern nötig
-if[[ $DEPENDENCIES == "1" ]]; then
-	sudo apt-get network-manager -y &&
-	echo "$TIMESTAMP: Network-Manager wurde installiert" >> $LOGFILE
-	sudo apt-get apparmor-utils -y &&
-	echo "$TIMESTAMP: Apparmor wurde installiert" >> $LOGFILE
-	sudo apt-get jq -y &&
-	echo "$TIMESTAMP: jq wurde installiert" >> $LOGFILE
-	sleep 3m
-	echo "$TIMESTAMP: Dependencies für HomeAssistant wurden installiert " >> $LOGFILE
+if [[ "$DEPENDENCIES" -eq 1 ]]; then
+		echo "$TIMESTAMP: Raspberry Pi 4 Dependencies werden installiert." >> $LOGFILE
+		sudo apt-get network-manager -y &&
+		echo "$TIMESTAMP: Network-Manager wurde installiert" >> $LOGFILE
+		sudo apt-get apparmor-utils -y &&
+		echo "$TIMESTAMP: Apparmor wurde installiert" >> $LOGFILE
+		sudo apt-get jq -y &&
+		echo "$TIMESTAMP: jq wurde installiert" >> $LOGFILE
+		sleep 3m
+		echo "$TIMESTAMP: Dependencies für HomeAssistant wurden installiert " >> $LOGFILE
 else
-	echo "$TIMESTAMP: Dependencies für Home Assistant nicht installiert - USERWAHL" >>$LOGFILE
+        echo "$TIMESTAMP: Dependencies für Home Assistant nicht installiert - USERWAHL" >>$LOGFILE
 fi
 #Installation von Home Assistant Supervised
-if[[ $HOMEASSISTANT == "1" ]; then
+if [[ "$HOMEASSISTANT" -eq 1 ]]; then
     sudo curl -Lo installer.sh https://raw.githubusercontent.com/home-assistant/supervised-installer/master/installer.sh
     sudo bash installer.sh --machine $MACHINE
-    echo "HomeAssistant Supervised wurden installiert $TIMESTAMP
-    Weboberflaeche des Home Assistant ist unter http://$PIADDRESS:8123 erreichbar
-    Bitte folgen sie für die Einrichtung und Installation von MQTT und Zigbee2MQTT
-    der Beiliegenden Anleitung" >> $LOGFILE
+    echo "$TIMESTAMP: HomeAssistant Supervised wurden installiert" >> $LOGFILE
+    echo "Weboberflaeche des Home Assistant ist unter http://$PIADDRESS:8123 erreichbar." >> $LOGFILE
+    echo "Bitte folgen sie fuer die Einrichtung und Installation von MQTT und Zigbee2MQTT der Beiliegenden Anleitung" >> $LOGFILE
     echo "
     +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     +                                                                           +
@@ -106,24 +109,25 @@ if[[ $HOMEASSISTANT == "1" ]; then
     +            für die installation von MQTT Broker und Zigbee2MQTT           +
     +                                                                           +
     +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-fi    
+fi
 # Hilfe-Funktion
-Help()
+# FUNKTIONIERT NIT!
+function Help()
 {
-	echo "   Smart Home Zentrale   "
-	echo "---Installationsskript---"
-	echo
-	echo "Das Installationsskript kann mit diversen Startparametern arbeiten."
-	echo "Die uebliche Syntax lautet:"
-	echo "  smz_install [-d/-ha/-u/-h]"
-	echo
-	echo "Die Parameter bewirken folgendes:"
-	echo " -d:   Installiert Docker falls nicht vorhanden"
-	echo " -u:   Aktualisiert das System mit apt update && apt upgrade -y"
-	echo " -ha:  Installiert Home Assistant Supervised"
-	echo " -nd:  Installierd die Dependencies NICHT."
-	echo
-	echo " -h:   Ruft diese Hilfeseite auf"
-	echo
-	echo "Der grundlegende Ablauf des Skripts wird in der Datei $LOGFILE geloggt."
+        echo "   Smart Home Zentrale   "
+        echo "---Installationsskript---"
+        echo
+        echo "Das Installationsskript kann mit diversen Startparametern arbeiten."
+        echo "Die uebliche Syntax lautet:"
+        echo "  smz_install [-d/-ha/-u/-h]"
+        echo
+        echo "Die Parameter bewirken folgendes:"
+        echo " -d:   Installiert Docker NICHT"
+        echo " -u:   Aktualisiert das System NICHT"
+        echo " -ha:  Installiert Home Assistant Supervised NICHT"
+        echo " -nd:  Installierd die Dependencies NICHT."
+        echo
+        echo " -h:   Ruft diese Hilfeseite auf"
+        echo
+        echo "Der grundlegende Ablauf des Skripts wird in der Datei $LOGFILE geloggt."
 }
